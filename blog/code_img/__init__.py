5d9309b970da3a3c7a65038d562977f513c6c9df
yuan yong <446506536@qq.com>
2018-08-11 13:05:36 +0800

    只保留Alipay

--- blog/code_img/__init__.py
+++
@@ -1,4 +0,0 @@
-#!user/bin/python3
-#Author:Mr.Yuan
-#-*- coding:utf-8 -*-
-#@time: 2018/7/17 20:06
--- s11luffycity/api/views/__init__.py
+++
@@ -1,5 +0,0 @@
-# !user/bin/python3
-# Author:Mr.Yuan
-# -*- coding:utf-8 -*-
-# @time: 2018/8/7 15:28
-
